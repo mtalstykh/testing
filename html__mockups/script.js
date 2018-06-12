55bf7ab61e09dd9c80325eb6720ccab252555eaa
Maksim Talstykh <maksimtalstykh@gmail.com>
2018-06-12 12:10:08 +0300

    Clear repo

--- html__mockups/script.js
+++
@@ -1,5 +0,0 @@
-document.querySelector(".loginContainer__signInButton").onclick = ()=>{
-	let styles = event.target.style;
-	styles.color = "#008AFF";
-	document.querySelector(".spinner").classList.remove("disabled");
-};
